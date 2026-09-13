abae4ea501070ff23eec46ab6bdd339adb8589e5
Shatabdi Datta Sristy <shatabdisds007@gmail.com>
2026-09-13 23:15:27 +0530

    Add calculator functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+let display = document.getElementById("display");
+
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        let value = button.innerText;
+
+        if (value === "C") {
+            display.value = "";
+        }
+        else if (value === "=") {
+            try {
+                display.value = eval(display.value);
+            } catch {
+                display.value = "Error";
+            }
+        }
+        else {
+            display.value += value;
+        }
+    });
+});
